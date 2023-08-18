007ec6381036914774359a700949374bd2a65616
AriadnaValencia <ariadna.valencia1999@gmail.com>
2023-08-17 21:16:18 -0300

    modificaciones para la tercer preentrega

--- index.js
+++ index.js
@@ -1,55 +1,126 @@
-//SEGUNDA PREENTREGA
-
-
-  class Producto {
-    constructor(nombre, precio, cantidad, categoria, id) {
-      this.nombre = nombre;
-      this.precio = precio;
-      this.cantidad = cantidad;
-      this.categoria = categoria;
-      this.id = id;
+//PRODUCTOS
+const productos = [
+    {
+        nombre: "Producto 1",
+        precio: 500,
+        descripcion: "Pinceles para nail art.",
+        imagen: "1.png"
+    },
+    {
+        nombre: "Producto 2",
+        precio: 2.000,
+        descripcion: "Top coat no cleanse. Elimina el pulido. Acabado espejo.",
+        imagen: "2.png"
+    },
+    {
+        nombre: "Producto 3",
+        precio: 1.800,
+        descripcion: "Set de pestañas para pxp.",
+        imagen: "3.png"
+    },
+    {
+        nombre: "Producto 4",
+        precio: 1.100,
+        descripcion: "Film para servicios de pestañas.",
+        imagen: "4.png"
+    },
+    {
+        nombre: "Producto 5",
+        precio: 800,
+        descripcion: "Vaso para humedecer pinceles para nail art y distintos usos en manicuria.",
+        imagen: "5.png"
+    },
+    {
+        nombre: "Producto 6",
+        precio: 1.000,
+        descripcion: "Cepillo para limpieza de polvo, luego del limado.",
+        imagen: "6.png"
+    },
+    {
+        nombre: "Producto 7",
+        precio: 2.100,
+        descripcion: "Aceite para cutículas. Restaurador.",
+        imagen: "7.png"
+    },
+    {
+        nombre: "Producto 8",
+        precio: 1.800,
+        descripcion: "Aceite de lavanda para humectar cutículas.",
+        imagen: "8.png"
+    },
+    {
+        nombre: "Producto 9",
+        precio: 2.500,
+        descripcion: "Polygel para construccion de esculpidas.",
+        imagen: "9.png"
+    },
+    {
+        nombre: "Producto 10",
+        precio: 1.800,
+        descripcion: "Henna. Ideal para sombreado de cejas.",
+        imagen: "10.png"
     }
-  }
+];
 
-  //ARRAY VACÍO
-  const productos = [];
+    //TARJETAS DE PRODUCTOS
+    productos.forEach(producto => {
+        const productoCard = document.createElement("div");
+        productoCard.className = "producto-card";
 
-let producto1 = new Producto ('Esmaltes', 1800, 80, 'Manicuria', 1);
-let producto2 = new Producto ('Repujadores', 1100, 50, 'Herramientas', 2);
-let producto3 = new Producto ('Limas', 400, 150, 'Herramientas', 3);
-let producto4 = new Producto ('Shampoo', 1650, 15, 'Peluquería', 4);
-let producto5 = new Producto ('Acondicionador', 1800, 30, 'Peluquería', 5);
+        const imagen = document.createElement("img");
+        imagen.src = producto.imagen;
+        imagen.alt = producto.nombre;
 
-agregarProducto(producto1)
-agregarProducto(producto2)
-agregarProducto(producto3)
-agregarProducto(producto4)
-agregarProducto(producto5)
+        const nombre = document.createElement("h3");
+        nombre.textContent = producto.nombre;
 
-  let productosOrdenados = productos.sort((a, b) => a.nombre.localeCompare(b.nombre));
+        const precio = document.createElement("p");
+        precio.textContent = `$${producto.precio.toFixed(2)}`;
 
-//PROCEDIMIENTO DE COMPRA
+        productoCard.appendChild(imagen);
+        productoCard.appendChild(nombre);
+        productoCard.appendChild(precio);
 
-const seleccionDeProducto = prompt('Hola! Para realizar su compra, puede elegir entre los siguientes productos: esmaltes, repujadores, limas, shampoo y acondicionadores');
+        productosContainer.appendChild(productoCard);
+    });
 
-
-const productoSeleccionado = productos.find((producto) => producto.nombre === productoSeleccionado);
-
-
-function comprarProducto(nombreProducto) {
-  const productoEncontrado = productos.find((producto) => producto.nombre === nombreProducto);
-  if (productoEncontrado) {
-    const precioProducto = productoEncontrado.precio;
-    alert(
-      `El producto "${productoEncontrado.nombre}" está preparado para ir a tu domicilio.` +
-      `Costo: $${precioProducto}` +
-      `¡Gracias por tu compra! Vuelva prontos`
-    );
-  } else {
-    alert('El producto seleccionado no está disponible en nuestra tienda.');
-  }
-}
-
-
-
-
+    //FUNCION PARA AGREGAR AL CARRITO
+    function generarTarjetas(productos) {
+        const productosContainer = document.getElementById("productos-container");
+    
+        const productosHTML = productos.reduce((acumulador, producto) => {
+            const productoCard = document.createElement("div");
+            productoCard.className = "producto-card";
+    
+            const imagen = document.createElement("img");
+            imagen.src = producto.imagen;
+            imagen.alt = producto.nombre;
+    
+            const nombre = document.createElement("h2");
+            nombre.textContent = producto.nombre;
+    
+            const precio = document.createElement("p");
+            precio.textContent = `$${producto.precio.toFixed(2)}`;
+    
+            const botonAgregar = document.createElement("button");
+            botonAgregar.textContent = "Agregar al Carrito";
+            botonAgregar.addEventListener("click", () => {
+                agregarAlCarrito(producto);
+            });
+    
+            productoCard.appendChild(imagen);
+            productoCard.appendChild(nombre);
+            productoCard.appendChild(precio);
+            productoCard.appendChild(botonAgregar);
+    
+            return acumulador + productoCard.outerHTML;
+        }, "");
+    
+        productosContainer.innerHTML = productosHTML;
+    }
+    
+    function agregarAlCarrito(producto) {
+        localStorage.setItem()
+    }
+    
+    generarTarjetas(productos);
